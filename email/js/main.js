8b4a0b0625e695bd2f579aa89bf5cf81a1877cb4
AngeloRoudani <ange.rouda@gmail.com>
2023-02-27 15:38:05 +0100

    email control in implementazione

---
+++ email/js/main.js
@@ -0,0 +1,23 @@
+/* 1 creazione di un array con diverse mail.
+   2 prompt della propria mail e successivamente un (if) per cui:
+        _SE la mail immessa è uguale a una delle mail nella lista allora stampa "accesso consentito"
+        _SE la mail immessa non corrisponde a nessuna mail nella lista allora stampa "accesso negato"
+*/
+
+let mailList = ["enrico@fermi.it" , "sauron@darklord.com" , "tony@stark.av" , "cassius@clay.us" , "edgarallan@poe.us" ];
+     
+const userMail = prompt("immetti la tua mail");
+
+for (let i = 0; i < mailList.length; i++) {
+
+    if (userMail = mailList[i]) {
+
+        console.log('Accesso consentito')
+
+    } else {
+
+        console.log('Accesso negato')
+        
+    }
+
+}
